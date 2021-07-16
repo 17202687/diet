0feca8404c6953a6e2c89847aa9d1be2c619397d
zzazan96 <zzazan96@naver.com>
2021-07-16 16:02:34 +0900

    file upload

--- cjb/thirdeyes/urls.py
+++ cjb/thirdeyes/urls.py
@@ -18,5 +18,18 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.login, name='login'),
+    path('main/', views.main, name='main'),
+    path('main/set/', views.set, name='set'),
+    path('signup/', views.signup, name='signup'),
+    path('main/set/user/', views.user, name='user'),
+    path('main/morning/', views.morning, name='morning'),
+    path('main/lunch/', views.lunch, name='lunch'),
+    path('main/dinner/', views.dinner, name='dinner'),
+    path('main/snack/', views.snack, name='snack'),
+    path('main/set/alarm/', views.alarm, name='alarm'),
+    path('main/diary/', views.diary, name='diary'),
+    path('main/set/alarm/malarm/', views.malarm, name='malarm'),
+    path('main/set/alarm/lalarm/', views.lalarm, name='lalarm'),
+    path('main/set/alarm/dalarm/', views.dalarm, name='dalarm'),
 ]
--- cjb/thirdeyes/views.py
+++ cjb/thirdeyes/views.py
@@ -2,5 +2,44 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
-    return render(request, 'thirdeyes/index.html')
+def login(request):
+    return render(request, 'thirdeyes/login.html')
+
+def main(request):
+    return render(request, 'thirdeyes/main.html')
+
+def set(request):
+    return render(request, 'thirdeyes/set.html')
+
+def signup(request):
+    return render(request, 'thirdeyes/signup.html')
+
+def user(request):
+    return render(request, 'thirdeyes/user.html')
+
+def lunch(request):
+    return render(request, 'thirdeyes/lunch.html')
+
+def dinner(request):
+    return render(request, 'thirdeyes/dinner.html')
+
+def morning(request):
+    return render(request, 'thirdeyes/morning.html')
+
+def snack(request):
+    return render(request, 'thirdeyes/snack.html')
+
+def alarm(request):
+    return render(request, 'thirdeyes/alarm.html')
+
+def diary(request):
+    return render(request, 'thirdeyes/diary.html')
+
+def malarm(request):
+    return render(request, 'thirdeyes/malarm.html')
+
+def lalarm(request):
+    return render(request, 'thirdeyes/lalarm.html')
+
+def dalarm(request):
+    return render(request, 'thirdeyes/dalarm.html')
